8f3ce1e6b8efc3dd28995d1b262dfd18f19d1cf5
Bishnu <bishnu.m@bizruntime.net>
2019-10-15 19:30:35 +0530

    C#

---
+++ 15-10-2019/Delegates/Declaration.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    class Declaration
+    {
+        public delegate void addnum(int a, int b);
+        public delegate void subnum(int a, int b);
+        public void sum(int a, int b)
+        {
+            Console.WriteLine("(100 + 40) = {0}", a + b);
+        }
+        public void subtract(int a, int b)
+        {
+            Console.WriteLine("(100 - 60) = {0}", a - b);
+        }
+        public static void Main(String[] args)
+        {
+            Declaration obj = new Declaration();
+
+            addnum obj1 = new addnum(obj.sum);
+            subnum obj2 = new subnum(obj.subtract);
+            obj1(100, 40);
+            obj2(100, 60);
+
+        }
+    }
+}
---
+++ 15-10-2019/Delegates/Multicasting.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    class Multicasting
+    {
+        public delegate void MultiDelegate(double height, double width);
+        public void area(double height, double width)
+        {
+            Console.WriteLine("Area is: {0}", (width * height));
+        }
+        public void perimeter(double height, double width)
+        {
+            Console.WriteLine("Perimeter is: {0} ", 2 * (width + height));
+        }
+        public static void Main(String[] args)
+        {
+
+            Multicasting rect = new Multicasting();
+
+            MultiDelegate rectdele = new MultiDelegate(rect.area);
+
+            rectdele += rect.perimeter;
+            rectdele.Invoke(6.3, 4.2);
+            Console.WriteLine();
+            rectdele.Invoke(16.3, 10.3);
+        }
+    }
+}
