83911b643c28c905576a36b7f26e92257ce3a3b4
Bishnu <bishnu.m@bizruntime.net>
2019-10-16 19:40:02 +0530

    C#

---
+++ 16-10-2019/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asynchronous
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Main();
+            Child();
+            Console.ReadKey();
+        }
+
+        public static async Task Main()
+        {
+            await Task.Run(() =>
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    Console.WriteLine(" Main");
+                }
+            });
+        }
+
+
+        public static void Child()
+        {
+            for (int i = 0; i < 30; i++)
+            {
+                Console.WriteLine("Child");
+            }
+        }
+    }
+
+}
+    
+
