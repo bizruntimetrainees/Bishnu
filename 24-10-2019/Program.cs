1f851321a61eb1cac6010052884214905014d391
Bishnu <bishnu.m@bizruntime.net>
2019-10-24 20:00:19 +0530

    C#

---
+++ 24-10-2019/Program.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSON
+{
+     class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            Student s = new Student();
+            Console.WriteLine("Enter Name");
+            s.Name = Console.ReadLine();
+            Console.WriteLine("Enter Id");
+            s.Id = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Degree");
+            s.Degree = Console.ReadLine();
+            
+            string strResultJson = JsonConvert.SerializeObject(s);
+            Console.WriteLine(strResultJson);
+
+
+            Student resultStudent = JsonConvert.DeserializeObject<Student>(strResultJson);
+            Console.WriteLine(string.Join(", ", resultStudent.ToString()));
+           // foreach (var de in resultStudent.ToString())
+           // {
+              //  Console.WriteLine(resultStudent.Name);
+              //  Console.WriteLine(resultStudent.Id);
+              //  Console.WriteLine(resultStudent.Degree);
+           // }
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ 24-10-2019/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSON
+{
+   public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Degree { get; set; }
+        
+
+        public override string ToString()
+        {
+            return string.Format("Student Information:\n\tId:{0},\n\tName:{1},\n\tDegree:{2}" 
+
+               ,Id,Name,Degree);
+        } 
+    }
+}
+
